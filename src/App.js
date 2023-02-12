477dcba27ff5035b9350dda51f25b72d470d4ece
olivier_dufour <olivier_dufour@ens.univ-artois.fr>
2023-02-12 08:19:55 +0100

    fix nav

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavBar from "./MyNavBar/MyNavBar";
 import AllQuotes from "./Quotes/AllQuotes";
 import OneQuote from "./Quotes/OneQuote";
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [showAll, setShowAll] = useState(false);
+
   return (
     <>
       <MyNavBar>
         <Routes>
-          <Route exact={true} path="/allQuotes" element={<AllQuotes />} />
+          <Route exact={true} path="/AllQuotes" element={<AllQuotes />} />
+         
           <Route exact={true} path="/oneQuote" element={<OneQuote />} />
         </Routes>
       </MyNavBar>
     </>
   );
 }
+
+// function App() {
+//   const [showAll, setShowAll] = useState(false);
+//   return (
+//     <div className="App">
+//       <h1>Mes citations !</h1>
+//       <button onClick={() => setShowAll(false)}>Unique</button>
+//       <button onClick={() => setShowAll(true)}>Tout</button>
+//       {/* <button onClick={() => removeQuote()}>Supprimer</button> */}
+//       {showAll ? <AllQuotes /> : <OneQuote />}
+//     </div>
+//   );
+// }
--- src/MyNavBar/MyNavBar.js
+++ src/MyNavBar/MyNavBar.js
@@ -1,20 +1,22 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container,Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function MyNavBar() {
-  <>
-    <Navbar bg="light" expand="lg">
-      coucou
-      <Container>
-        coucou 2
-        <Nav.Item>
-          <Link to="/allQuotes"> Les citations </Link>
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        coucou
+        <Container>
+          coucou 2
+          <Nav.Link href="/AllQuotes">Les citations</Nav.Link>
+          <Nav.Item>
         </Nav.Item>
-        <Nav.Item>
+        {/* <Nav.Item>
           <Link to="/oneQuote"> Une citation</Link>
-        </Nav.Item>
-      </Container>
-    </Navbar>
-  </>;
+        </Nav.Item> */}
+        </Container>
+      </Navbar>
+    </>
+  );
 }
